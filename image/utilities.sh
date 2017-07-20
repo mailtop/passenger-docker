15eed6ea87fab1806ea9fdb8ada0ef785b7a9856
Rodrigo de Avila <rodrigo.avila@mailtop.com.br>
2017-07-20 18:07:57 -0300

    Adiciona instalação do wkhtmltopdf

--- image/utilities.sh
+++ image/utilities.sh
@@ -6,3 +6,9 @@ source /pd_build/buildconfig
 run minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
 run minimal_apt_get_install git
+
+## Install wkhtmltopdf from Github
+run curl -fsSL https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz -o wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+run tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+run mv wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+run rm -rf wkhtmltox*
