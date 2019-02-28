b2ebb84bd6163b909291e4a3972f40d1d8f993ca
Rodrigo de Avila <rodrigo.avila@mailtop.com.br>
2019-02-28 19:59:13 -0300

    Adiciona wkhtmltoimage

--- image/utilities.sh
+++ image/utilities.sh
@@ -7,10 +7,10 @@ run minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
 run minimal_apt_get_install git
 # Other dependencies
-run minimal_apt_get_install imagemagick libcurl4-openssl-dev
+run minimal_apt_get_install imagemagick libcurl4-openssl-dev libfontenc1 xfonts-75dpi xfonts-base xfonts-encodings xfonts-utils fontconfig libxrender1
 
 ## Install wkhtmltopdf from Github
-run curl -fsSL https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz -o wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-run tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-run mv wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-run rm -rf wkhtmltox*
+run curl -fsSL https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb -o wk.deb
+run dpkg -i wk.deb
+run rm -rf wk.deb
+
